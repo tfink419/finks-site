f0b80495f1a714a48204fe317875b77d852e5606
Tyler Fink <tfink419@gmail.com>
2023-04-24 11:04:04 -0600

    Remove privacy policy

--- src/python/main.py
+++ src/python/main.py
@@ -15,10 +15,6 @@ def index():
     global siteData
     return render_template('index.html', siteData=siteData)
 
-@app.route('/privacy_policy')
-def privacy_policy():
-    return render_template('privacy_policy.html')
-    
 if __name__ == '__main__':
     if os.environ.get('PORT'):
         app.run(host='0.0.0.0', debug=False, port=PORT)
